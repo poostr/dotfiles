96d9680eee42cc4e32b96e63365aeeadd184c982
poostr <poostrprog@gmail.com>
2024-08-21 13:44:28 +0300

    fix: oil preview and hide ignored dirs

--- nvim/.config/nvim/lua/postr/plugins/oil.lua
+++ nvim/.config/nvim/lua/postr/plugins/oil.lua
@@ -3,22 +3,50 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local oil = require("oil")
-    local detail = false
+		local detail = false
 
-		vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "open dir" })
+		vim.keymap.set("n", "<leader>e", function()
+			oil.open()
+			-- Wait until oil has opened, for a maximum of 1 second.
+			vim.wait(1000, function()
+				return oil.get_cursor_entry() ~= nil
+			end)
+			if oil.get_cursor_entry() then
+				oil.open_preview()
+			end
+		end, { desc = "open dir" })
 
 		oil.setup({
+			prompt_save_on_select_new_entry = true,
 			default_file_explorer = true,
+			constrain_cursor = "editable",
 			delete_to_trash = true,
 			skip_confirm_for_simple_edits = true,
 			view_options = {
-				show_hidden = true,
+				is_hidden_file = function(name, _)
+					if vim.startswith(name, "__p") then
+						return true
+          elseif vim.startswith(name, '.idea') then
+            return true
+          elseif vim.startswith(name, '.DS') then
+            return true
+          elseif vim.startswith(name, '.ruff') then
+            return true
+          elseif vim.startswith(name, '.pytest') then
+            return true
+          elseif vim.startswith(name, '.g') then
+            return true
+					end
+				end,
 			},
 			float = {
 				preview_split = "left",
 				override = function(conf)
 					return conf
 				end,
+			},
+			preview = {
+				update_on_cursor_moved = true,
 			},
 			keymaps = {
 				["gd"] = {
